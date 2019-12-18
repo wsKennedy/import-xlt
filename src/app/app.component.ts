98245ffd2f207e86e9acdc837e12d0b0c1df69ba
Kennedy Lima <kennedy.lima@ipdec.org>
2019-12-18 14:57:36 -0400

    importação de xls para outras extenções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'import-xls';
+
+  storeData: any;
+  csvData: any;
+  jsonData: any;
+  textData: any;
+  htmlData: any;
+  fileUploaded: File;
+  worksheet: any;
+
+  uploadedFile(event) {
+    this.fileUploaded = event.target.files[0];
+    this.readExcel();
+  }
+
+  readExcel() {
+    const readFile = new FileReader();
+    readFile.onload = (e) => {
+      this.storeData = readFile.result;
+      const data = new Uint8Array(this.storeData);
+      const arr = new Array();
+      for (let i = 0; i !== data.length; ++i) { arr[i] = String.fromCharCode(data[i]); }
+      const bstr = arr.join('');
+      const workbook = XLSX.read(bstr, { type: 'binary' });
+      const SHEET_NAME: string[] =  workbook.SheetNames;
+      this.worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    };
+    readFile.readAsArrayBuffer(this.fileUploaded);
+  }
+
+  readAsCSV() {
+    this.csvData = XLSX.utils.sheet_to_csv(this.worksheet);
+    const data: Blob = new Blob([this.csvData], { type: 'text/csv;charset=utf-8;' });
+    FileSaver.saveAs(data, 'CSVFile' + new Date().getTime() + '.csv');
+  }
+
+  readAsJson() {
+    this.jsonData = XLSX.utils.sheet_to_json(this.worksheet, { raw: false });
+    this.jsonData = JSON.stringify(this.jsonData);
+    const data: Blob = new Blob([this.jsonData], { type: 'application/json' });
+    FileSaver.saveAs(data, 'JsonFile' + new Date().getTime() + '.json');
+  }
+
+  readAsHTML() {
+    this.htmlData = XLSX.utils.sheet_to_html(this.worksheet);
+    const data: Blob = new Blob([this.htmlData], { type: 'text/html;charset=utf-8;' });
+    FileSaver.saveAs(data, 'HtmlFile' + new Date().getTime() + '.html');
+  }
+
+  readAsText() {
+    this.textData = XLSX.utils.sheet_to_txt(this.worksheet);
+    const data: Blob = new Blob([this.textData], { type: 'text/plain;charset=utf-8;' });
+    FileSaver.saveAs(data, 'TextFile' + new Date().getTime() + '.txt');
+  }
+
 }
